f1de324d6b93ace6867cc8a98a75ab625fdbc2ac
miroslav <miroslav>
2020-09-19 21:45:28 +0200

    Added files and changes required for deployment to heroku

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log('Server is running...');
 });
